e77665aff8395dd89d4b0c515f7231368bcae04c
Ritukumari2003 <2503.ritu@gmail.com>
2026-02-16 12:10:14 +0530

    blogs and search features

--- blog_main/views.py
+++ blog_main/views.py
@@ -1,11 +1,21 @@
 from django.shortcuts import render
+from about.models import About
 from blogs.models import Category, Blog
 
 def home(request):
     featured_posts = Blog.objects.filter(is_featured=True, status = 'Published').order_by('updated_at')
     posts = Blog.objects.filter(is_featured=False, status = 'Published').order_by('updated_at')
+
+    # Fetch the about section data 
+    try:
+        about = About.objects.get()
+    except:
+        about=None
+        
     context = {
         'featured_posts': featured_posts,
         'posts':posts,
+        'about': about,
     }
+
     return render(request, 'home.html', context)
