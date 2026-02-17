2a6aa9eaf95e70c4ec7b02dcc091dd8b333a6374
Ritukumari2003 <2503.ritu@gmail.com>
2026-02-17 15:19:54 +0530

    category crud functionalities edited

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import get_object_or_404, redirect, render
 from blogs.models import Blog, Category
 from django.contrib.auth.decorators import login_required
-
+from django.contrib import messages
 from .forms import CategoryForm
 
 # Create your views here.
@@ -20,15 +20,18 @@ def categories(request):
 
 def add_category(request):
     if request.method == 'POST':
-        form=CategoryForm(request.POST)
+        form = CategoryForm(request.POST)
         if form.is_valid():
             form.save()
             return redirect('categories')
-    form = CategoryForm()
+    else:
+        form = CategoryForm()
+
     context = {
-        'form': form,
-    }
+            'form': form,
+        }
     return render(request, 'dashboard/add_category.html', context)
+
 
 def edit_category(request, pk):
     category = get_object_or_404(Category, pk=pk)
